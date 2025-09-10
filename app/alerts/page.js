5cd59de6d1c587b44838c7ed815fa0cb918543eb
itsriddhima <riddhimasingh0707@gmail.com>
2025-09-10 17:37:17 +0530

    alerts update - page.js
    alerts amde more readable

--- app/alerts/page.js
+++ app/alerts/page.js
@@ -1,8 +1,14 @@
 export default function Alerts() {
   return (
     <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4 text-orange-400">Emergency Alerts</h1>
-      <p className="text-gray-300">Real-time alerts (Floods, Earthquakes, etc.) will be shown here.</p>
+      <h1 className="text-3xl font-extrabold mb-4 text-orange-400">
+        Emergency Alerts
+      </h1>
+      <p className="text-gray-300 text-lg">
+        Real-time alerts such as <span className="font-semibold">floods</span>,{" "}
+        <span className="font-semibold">earthquakes</span>, and other disasters
+        will be displayed here.
+      </p>
     </div>
   );
 }
